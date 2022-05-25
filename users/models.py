7ee28ac071d77bd0fff6671ac2a76427d378caae
jaeseung <jhwang90801@gmail.com>
2022-05-25 15:51:50 +0900

    modified(2) - wesulloc modeling

--- users/models.py
+++ users/models.py
@@ -14,9 +14,9 @@ class User(TimeStampModel):
     password         = models.CharField(max_length=150)
     name             = models.CharField(max_length=45)
     address          = models.CharField(max_length=100)
-    contact          = models.CharField(max_length=45)
+    contact          = models.CharField(max_length=45, unique=True)
     birth            = models.DateField()
-    email            = models.CharField(max_length=100, unique=True)
+    email            = models.CharField(max_length=50, unique=True)
     gender           = models.CharField(max_length=30)
     terms_agreements = models.JSONField()
 

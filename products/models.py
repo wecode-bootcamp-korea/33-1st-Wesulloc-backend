b9ee33007ff722698dd16d6ca9d7f20667906bf7
jaeseung <jhwang90801@gmail.com>
2022-05-26 18:52:37 +0900

    modified: (ADD) category_product table in Models

--- products/models.py
+++ products/models.py
@@ -26,8 +26,14 @@ class Category(models.Model):
     class Meta:
         db_table = "categories"
 
+class CategoryProduct(models.Model): 
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    product  = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "categories_products"
+
 class Product(models.Model):
-    category      = models.ForeignKey('Category', on_delete=models.CASCADE)
     name          = models.CharField(max_length=50)
     price         = models.DecimalField(max_digits=10, decimal_places=0)
     description   = models.TextField()

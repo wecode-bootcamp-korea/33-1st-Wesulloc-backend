d25620d28880424e4c0ceae9e426169c9b0694cd
jaeseung <jhwang90801@gmail.com>
2022-05-27 21:27:26 +0900

    modified(2): productList view(draft ver.)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import json
+from json.decoder     import JSONDecodeError
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q, Count, F
+
+from products.models  import Product, Category, MainCategory
+
+
+class ProductListView(View):
+    def get(self, request):
+        try:
+            menu          = request.GET.get('menu', None)
+            main_category = request.GET.get('main_category', None)
+            category      = request.GET.get('category', None)
+            searching     = request.GET.get('name')
+            sort          = request.GET.get('sort', 'new')
+            limit         = int(request.GET.get('limit', 4))
+            offset        = int(request.GET.get('offset',0))
+
+            q = Q()
+            
+            if menu:
+                q &= Q(categoryproduct__category__main_category__menu__id = menu)
+
+            if main_category:
+                q &= Q(categoryproduct__category__main_category = main_category)
+
+            if category:
+                q &= Q(categoryproduct__category = category)
+
+            if searching:
+                q &= Q(name_icontains=searching)
+
+            sort_type = {
+                'reviews'   : '-total_reviews',
+                'sales'     : '-total_sales',
+                'new'       : '-id',
+                'high_price': '-price',
+                'low_price' : 'price'
+                }
+
+            products = Product.objects.filter(q)\
+                        .annotate(total_sales=Count('orderitem__id') * F('orderitem__quantity')\
+                        , total_reviews=Count('review__id')).distinct()\
+                        .order_by(sort_type.get(sort))[offset:offset+limit]
+
+            products_list = [{
+                    "id"           : product.id,
+                    "name"         : product.name,
+                    "price"        : product.price,
+                    "discount_rate": product.discount_rate,
+                    "new"          : True if product in Product.objects.all().order_by('-id')[:2] else False,
+                    "sale_or_not"  : False if product.discount_rate == 0 else True
+                    } for product in products]
+        
+            return JsonResponse({'results': products_list}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"message" : "PRODUCT_DOES_NOT_EXIST"}, status = 401)

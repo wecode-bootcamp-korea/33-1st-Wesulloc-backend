ba99a3ca6d3b3bf08393ab6d39f92f948cfbf1dc
jaeseung <jhwang90801@gmail.com>
2022-05-30 20:56:33 +0900

    modified(8): productDetail View
    - App 추가 생성 이후, productList Branch에서 merge 받아옴
    - 제품 상세정보, 추천 상품 추가
    - Carts 앱 views.py 작성을 위해 다시 커밋 진행

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.models     import User
+from products.models  import ProductImage
+from carts.models     import Cart
+
+"""장바구니 구현 관련 (CART 구현)"""
+"""
+[ {
+    id: 1,
+    name: '영귤섬 아이스티',
+    packingState: '포장불가',
+    price: 13000,
+    amount: 1,
+  },
+]
+"""
+class CartView(View):
+    #@login_decorator
+    def get(self, request):
+        try:
+            # user = request.user
+            carts = Cart.objects.filter(user_id=1)
+
+            cart_list = [{
+                # "user_id" : user.id,
+                "cart_id" : cart.id,
+                "product_id" : cart.product.id,
+                "product_name" : cart.product.name,
+                "product_img" : ProductImage.objects.get(id=cart.product.id).img_url,
+                "price" : cart.product.price,
+                "quantity" : cart.quantity,
+            } for cart in carts]
+
+            return JsonResponse({"cart_list" : cart_list}, status=200)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    
+    # @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            # user = request.user
+            user = User.objects.get(id=1)
+            product_id = int(data['product_id'])
+            quantity = int(data['quantity'])
+
+            cart, created = Cart.objects.get_or_create(
+                user_id = user.id,
+                product_id = product_id,
+                quantity = quantity
+            )
+            cart.save()
+            return JsonResponse({"message" : "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+        except Cart.DoesNotExist:
+            return JsonResponse({"message" : "CART_DOES_NOT_EXIST"}, status=400)
+            
+
+
+            return JsonResponse
+
+        except KeyError:
+            return JsonResponse
+
+    def patch(self, request):
+
+        return
+
+    def delete(self, request):
+
+        return

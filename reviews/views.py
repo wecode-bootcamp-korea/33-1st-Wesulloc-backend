d82d13d9f5707baf80a8a2e58412c64d70d226ef
jaeseung <jhwang90801@gmail.com>
2022-05-31 21:17:56 +0900

    CREATE: review View

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+from core.utils       import access_token_check
+
+from users.models     import User
+from products.models import Product
+from reviews.models     import Review
+
+
+class ReviewView(View):
+    @access_token_check
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = request.user
+            product_id = data['product_id']
+            rating = data['rating']
+            content = data['content']
+
+            product = Product.objects.get(id = product_id)
+
+            Review.objects.create(
+                user_id = user.id,
+                product_id = product_id,
+                content = content,
+                rating = rating
+            )
+            return JsonResponse({"message" : "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"message" : "PRODUCT_DOES_NOT_EXIST"}, status=404)
+
+    # def get(self, request, product_id):
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(TimeStampModel):
     account          = models.CharField(max_length=45, unique=True)
     password         = models.CharField(max_length=150)
     name             = models.CharField(max_length=45)
-    address          = models.CharField(max_length=100)
+    address          = models.CharField(max_length=100, null=True)
     contact          = models.CharField(max_length=45, unique=True)
     birth            = models.DateField()
     email            = models.CharField(max_length=50, unique=True)

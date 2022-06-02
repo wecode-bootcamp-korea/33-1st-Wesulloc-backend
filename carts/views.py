56806ff59e12fadcfa0d9fd327c4158ecab3dc84
jaeseung <jhwang90801@gmail.com>
2022-06-02 16:14:53 +0900

    modified(6): carts View
    - Carts 리뷰 확인

--- carts/views.py
+++ carts/views.py
@@ -4,7 +4,6 @@ from django.http      import JsonResponse
 from django.views     import View
 from core.utils       import access_token_check
 
-from users.models     import User
 from carts.models     import Cart
 
 class CartView(View):
@@ -12,14 +11,13 @@ class CartView(View):
     def get(self, request):
         user = request.user
         carts = Cart.objects.filter(user_id = user.id)
-        # carts = Cart.objects.prefetch_related('product__productimage_set').filter(user_id = user.id)
 
         cart_list = [{
             "user_id" : user.id,
             "cart_id" : cart.id,
             "product_id" : cart.product.id,
             "product_name" : cart.product.name,
-            "product_img" : cart.product.productimage_set.all()[0].img_url,
+            "product_img" : cart.product.productimage_set.first().img_url,
             "price" : cart.product.price,
             "quantity" : cart.quantity,
         } for cart in carts]
@@ -27,7 +25,6 @@ class CartView(View):
     
     @access_token_check
     def post(self, request):
-        # POST /carts
         try:
             data = json.loads(request.body)
             user = request.user          
@@ -51,7 +48,6 @@ class CartView(View):
     
     @access_token_check
     def patch(self, request, cart_id):
-        # PATCH /carts/1
         try:
             data = json.loads(request.body)
 
@@ -72,31 +68,9 @@ class CartView(View):
 
     @access_token_check
     def delete(self, request):
-        # # DELETE /carts?cart_id=29&cart_id=30
-        # # or
-        # # DELETE /carts?ids=[1,2,3]
-        # user = request.user.id
-        # cart_ids = request.GET.get('cart_ids')
-        # print(type(cart_ids)) # str
-        # print(cart_ids) #'[1,2,3]'
-        # print(type(json.loads(cart_ids))) #[1,2,3]
-        # print(cart_ids)
-
+        user = request.user.id
         cart_ids = request.GET.getlist('cart_id')
 
         Cart.objects.filter(id__in=cart_ids, user_id=user).delete()
 
         return JsonResponse({"message" : "SUCCESS"}, status=200)
-
-    ###### 멘토님과 함께 짠 코드    
-    # @access_token_check
-    # def delete(self, request):
-    #     DELETE /carts?ids=[1,2,3] ??????
-    #     DELETE /carts?ids=1,2,3
-    #     DELETE /carts?ids=1&ids=2&ids=3
-    #     user = request.user.id
-    #     cart_ids = request.GET.getlist('cart_ids')
-
-    #     Cart.objects.filter(id__in=cart_ids, user_id=user).delete()
-
-    #     return JsonResponse({"message" : "SUCCESS"}, status=200)
